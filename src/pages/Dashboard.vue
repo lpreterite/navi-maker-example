25fa60cf692767f7a06801b1b28286b88e957c3c
Packy <lpreterite@126.com>
2024-03-19 17:02:45 +0800

    各个页面增加展示内容

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,11 +1,90 @@
 <template>
   <div class="container">
     <h2>仪表盘</h2>
-    <div>...</div>
+    <div class="dashboard-card">
+      <div v-for="group in groups" :key="group.title">
+        <el-card>
+          <div slot="header">
+            <span>{{ group.title }}</span>
+          </div>
+          <template v-for="content in group.children">
+            <router-link
+              :to="{ path: content.path }"
+              class=""
+              :key="content.title"
+            >
+              <i :class="content.icon"></i>
+              <span>{{ content.title }}</span>
+            </router-link>
+          </template>
+        </el-card>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      groups: [
+        {
+          title: "文章管理",
+          children: [
+            {
+              title: "草稿箱",
+              icon: "el-icon-document",
+              path: "/posts/draf",
+            },
+            {
+              title: "已发布",
+              icon: "el-icon-document-checked",
+              path: "/posts/publish",
+            },
+            {
+              title: "垃圾桶",
+              icon: "el-icon-delete",
+              path: "/posts/trash",
+            },
+          ],
+        },
+        {
+          title: "用户管理",
+          children: [
+            {
+              title: "用户列表",
+              icon: "el-icon-user",
+              path: "/users",
+            },
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
+<style lang="scss" scoped>
+.dashboard-card {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  > div {
+    padding: 0 1em;
+    flex-grow: 1;
+    width: 25%;
+  }
+  i{
+    display: block;
+    font-size: 32px;
+    text-align: center;
+    color: #409eff;
+  }
+  a{
+    display: inline-block;
+    margin: 0.5em 0.5em 0.5em 0;
+    color: #555;
+    text-decoration: none;
+    font-size: 14px;
+  }
+}
+</style>
--- src/pages/Publish.vue
+++ src/pages/Publish.vue
@@ -1,11 +1,77 @@
 <template>
   <div class="container">
-    <h2>发布库</h2>
-    <div>...</div>
+    <h2>已发布</h2>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column fixed prop="pid" label="PID" width="150">
+      </el-table-column>
+      <el-table-column prop="title" label="文章标题"> </el-table-column>
+      <el-table-column prop="tags" label="标签">
+        <template slot-scope="scope">
+          <template v-for="tag in scope.row.tags">
+            <el-tag
+              :key="tag.name"
+              size="medium"
+              style="margin: 0.5em 1em 0.5em 0"
+              >{{ tag }}</el-tag
+            >
+          </template>
+        </template>
+      </el-table-column>
+      <el-table-column prop="created_at" label="创建时间" width="120">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
 export default {
   name: "Publish",
+  data() {
+    return {
+      tableData: [
+        {
+          pid: "12987122",
+          created_at: "2016-05-02",
+          title: "男子被流浪猫绊倒 投喂者赔24万热",
+          tags: ["猫", "宠物"],
+        },
+        {
+          pid: "12987123",
+          created_at: "2016-05-04",
+          title: "许家印被限制高消费热",
+          tags: ["消费"],
+        },
+        {
+          pid: "12987125",
+          created_at: "2016-05-01",
+          title: "春来种下丰产粮",
+          tags: ["粮", "种田"],
+        },
+        {
+          pid: "12987126",
+          created_at: "2016-05-03",
+          title: "北大回应考生与前校长蔡元培同名",
+          tags: ["北大", "蔡元培"],
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(item) {
+      this.$router.push({
+        name: "posts/publish/detail",
+        params: {
+          id: item.pid,
+        },
+      });
+    },
+  },
 };
 </script>
